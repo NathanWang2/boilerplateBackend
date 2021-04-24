fc05ae217050edc2de34d59ea1c952994b056c6c
Nathan Wang <nathan.wang2@gmail.com>
2021-04-24 16:48:31 -0700

    Just adding a bit of comments

--- be/__init__.py
+++ be/__init__.py
@@ -5,4 +5,5 @@ app = Flask(__name__)
 app.config['MONGO_DBNAME'] = "mongodb://localhost:27017"
 app.config['MONGO_URI'] = 'TodoDB'
 
+# This is where you want to add your new endpoints.
 app.register_blueprint(list_blueprint)
